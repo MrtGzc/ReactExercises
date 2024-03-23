481644e134203e7e281c0444d4a890eddeeb3a5a
Mert Gezici <mrt_gzc@outlook.com>
2024-03-23 04:34:50 +0300

    Implemented functionality to replace learned words with known words and to list them when necessary

--- myWordNoteBookProject/src/App.jsx
+++ myWordNoteBookProject/src/App.jsx
@@ -6,13 +6,15 @@ import AddWordComponent from "./components/AddWordComponent";
 function App() {
   const [translateStatus, settranslateStatus] = useState(false);
   const [addStatus, setaddStatus] = useState(false);
+  const [learnedWords, setlearnedWords] = useState([]);
+  const [showLearnedWordsStatus, setshowLearnedWordsStatus] = useState(false);
   const [words, setWords] = useState([
     {
       id: "1",
-      wordTr: "merhaba",
-      wordEng: "hello",
-      sentenceTr: "I said hello to him",
-      sentenceEng: "Ona merhaba dedim",
+      word: "merhaba",
+      eng: "hello",
+      sentence: "I said hello to him",
+      turkish_sentence: "Ona merhaba dedim",
     },
   ]);
   const [wordNumber, setwordNumber] = useState(0);
@@ -28,84 +30,140 @@ function App() {
   const getWords = async () => {
     const response = await axios.get("http://localhost:3000/unknownwords");
     setWords(response.data);
+
+    const LearnedWordsResponse = await axios.get(
+      "http://localhost:3000/knownwords"
+    );
+    setlearnedWords(LearnedWordsResponse.data);
   };
 
   const nextWord = () => {
+    if (!showLearnedWordsStatus) {
+      if (wordNumber + 1 == words.length) {
+        setwordNumber(0);
+      } else {
+        setwordNumber(wordNumber + 1);
+      }
+    } else {
+      if (wordNumber + 1 == learnedWords.length) {
+        setwordNumber(0);
+      } else {
+        setwordNumber(wordNumber + 1);
+      }
+    }
+  };
+  const learned = async () => {
+    await axios.post("http://localhost:3000/knownwords", {
+      wordTr: words[wordNumber].wordTr,
+      wordEng: words[wordNumber].wordEng,
+      sentenceTr: words[wordNumber].sentenceTr,
+      sentenceEng: words[wordNumber].sentenceEng,
+    });
+    await axios.delete(
+      `http://localhost:3000/unknownwords/${words[wordNumber].id}`
+    );
+    getWords();
     if (wordNumber + 1 === words.length) {
       setwordNumber(0);
-    } else {
-      setwordNumber(wordNumber + 1);
     }
   };
 
-  const learned = async () => {
-    if (words.length === 0) {
+  const forgotClick = async () => {
+    await axios.delete(
+      `http://localhost:3000/knownwords/${learnedWords[wordNumber].id}`
+    );
+    if (wordNumber + 1 === learnedWords.length) {
       setwordNumber(0);
-      return;
     }
-    if(words.length === wordNumber+1){
-      setwordNumber(wordNumber-1)
-    }
-
-    const currentWordNumber =
-      wordNumber >= words.length ? words.length - 1 : wordNumber;
-    const currentWord = words[currentWordNumber];
-
-    if (!currentWord) {
-      // Eğer currentWord undefined ise, işlemi sonlandır
-      return;
-    }
-
-    await axios.delete(`http://localhost:3000/unknownwords/${currentWord.id}`);
-    getWords();
-
-    if (currentWordNumber === words.length - 1) {
-      setwordNumber(0);
-    } else {
-      setwordNumber(currentWordNumber);
-    }
-
-    await axios.post("http://localhost:3000/knownwords", {
-      wordTr: currentWord.wordTr,
-      wordEng: currentWord.wordEng,
-      sentenceTr: currentWord.sentenceTr,
-      sentenceEng: currentWord.sentenceEng,
+    await axios.post("http://localhost:3000/unknownwords", {
+      wordTr: learnedWords[wordNumber].wordTr,
+      wordEng: learnedWords[wordNumber].wordEng,
+      sentenceTr: learnedWords[wordNumber].sentenceTr,
+      sentenceEng: learnedWords[wordNumber].sentenceEng,
     });
+    getWords();
+  };
+
+  const showLearnedWords = () => {
+    setshowLearnedWordsStatus(!showLearnedWordsStatus);
+    setwordNumber(0);
   };
 
   useEffect(() => {
     getWords();
-  }, [learned]);
-
+  }, []);
   return (
-    <div className="appContainer">
-      <button className="addButton" onClick={addWordStatus}>
-        Yeni Bir Kelime Ekle
-      </button>
-      <button className="learnedWordsBtn">Öğrenilen Kelimeleri Göster</button>
-      {addStatus ? <AddWordComponent></AddWordComponent> : <div></div>}
-      <p className="wordTurn">
-        {wordNumber + 1}/{words.length}
-      </p>
-      <h1 className="wordTr">{words[wordNumber].wordTr}</h1>
-      {translateStatus ? (
-        <div>
-          <h4 className="wordEng">{words[wordNumber].wordEng}</h4>
-          <h5 className="sentenceEng">{words[wordNumber].sentenceEng}</h5>
-          <h5 className="sentenceTr">{words[wordNumber].sentenceTr}</h5>
+    <div>
+      {showLearnedWordsStatus ? (
+        <div className="appContainer">
+          <button className="addButton" onClick={addWordStatus}>
+            Yeni Bir Kelime Ekle
+          </button>
+          <button className="learnedWordsBtn" onClick={showLearnedWords}>
+            Öğrenilen Kelimeleri Göster
+          </button>
+          {addStatus ? <AddWordComponent></AddWordComponent> : <div></div>}
+          <p className="wordTurn">
+            {wordNumber + 1}/{learnedWords.length}
+          </p>
+          <h1 className="wordTr">{learnedWords[wordNumber].wordTr}</h1>
+          {translateStatus ? (
+            <div>
+              <h4 className="wordEng">{learnedWords[wordNumber].wordEng}</h4>
+              <h5 className="sentenceEng">
+                {learnedWords[wordNumber].sentenceEng}
+              </h5>
+              <h5 className="sentenceTr">
+                {learnedWords[wordNumber].sentenceTr}
+              </h5>
+            </div>
+          ) : (
+            <div></div>
+          )}
+          <div className="btns">
+            <button onClick={forgotClick}>Unuttum</button>
+            {translateStatus ? (
+              <button onClick={showTranslate}>Çevirisi Kapat</button>
+            ) : (
+              <button onClick={showTranslate}>çevirisini göster</button>
+            )}
+            <button onClick={nextWord}>devam et</button>
+          </div>
         </div>
       ) : (
-        <div></div>
+        // burdan sonra false ise
+        <div className="appContainer">
+          <button className="addButton" onClick={addWordStatus}>
+            Yeni Bir Kelime Ekle
+          </button>
+          <button className="learnedWordsBtn" onClick={showLearnedWords}>
+            Öğrenilen Kelimeleri Göster
+          </button>
+          {addStatus ? <AddWordComponent></AddWordComponent> : <div></div>}
+          <p className="wordTurn">
+            {wordNumber + 1}/{words.length}
+          </p>
+          <h1 className="wordTr">{words[wordNumber].wordTr}</h1>
+          {translateStatus ? (
+            <div>
+              <h4 className="wordEng">{words[wordNumber].wordEng}</h4>
+              <h5 className="sentenceEng">{words[wordNumber].sentenceEng}</h5>
+              <h5 className="sentenceTr">{words[wordNumber].sentenceTr}</h5>
+            </div>
+          ) : (
+            <div></div>
+          )}
+          <div className="btns">
+            <button onClick={learned}>Öğrendim</button>
+            {translateStatus ? (
+              <button onClick={showTranslate}>Çevirisi Kapat</button>
+            ) : (
+              <button onClick={showTranslate}>çevirisini göster</button>
+            )}
+            <button onClick={nextWord}>devam et</button>
+          </div>
+        </div>
       )}
-      <div className="btns">
-        <button onClick={learned}>Öğrendim</button>
-        {translateStatus ? (
-          <button onClick={showTranslate}>Çevirisi Kapat</button>
-        ) : (
-          <button onClick={showTranslate}>çevirisini göster</button>
-        )}
-        <button onClick={nextWord}>devam et</button>
-      </div>
     </div>
   );
 }
